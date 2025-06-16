2641a6f8b75263269d56b627611aa3f6eaf4561e
Vitaliy <vitaliyufc@gmail.com>
2025-06-16 21:22:46 +0300

    fixed project

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -9,7 +9,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class HibernateUtil {
     private static SessionFactory sessionFactory = buildSessionFactory();
 
-    public HibernateUtil() {
+    private HibernateUtil() {
 
     }
 
